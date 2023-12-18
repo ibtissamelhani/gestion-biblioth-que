1f904952f785f4e3bc925b2219b21cf7979b50a6
ibtissam <ibtissamelhani36@gmail.com>
2023-12-18 22:05:40 +0100

    add login an signup front end

---
+++ Views/register.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Design by foolishdeveloper.com -->
+    <title>Glassmorphism login Form Tutorial in html css</title>
+ 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/authStyle.css">
+    <!--Stylesheet-->
+    <!-- <style media="screen">
+
+    </style> -->
+</head>
+<body>
+    <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div>
+    <form>
+        <h3>Login Here</h3>
+
+        <label for="username">first name</label>
+        <input type="text" placeholder="first name" id="username">
+
+        <label for="username">last name</label>
+        <input type="text" placeholder="last name" id="username">
+
+        <label for="username">email</label>
+        <input type="text" placeholder="Email " id="username">
+
+        <label for="username">phone</label>
+        <input type="text" placeholder="Phone" id="username">
+
+        <label for="password">Password</label>
+        <input type="password" placeholder="Password" id="password">
+
+        <label for="password">repeat assword</label>
+        <input type="password" placeholder="repeat Password" id="password">
+
+        <button>create account</button>
+        
+    </form>
+</body>
+</html>
