c2c598b8ea81d03c305de8bb06ab6e39181f8f76
ibtissam <ibtissamelhani36@gmail.com>
2023-12-18 17:12:28 +0100

    connect data base

--- App/DataBase/connection.php
+++ App/DataBase/connection.php
@@ -1,14 +1,33 @@
 <?php
- class dataBaseConnection {
-    public static function connection(){
-        
+namespace App\DataBase;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+use Dotenv\Dotenv;
+use PDO;
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
+
+
+ class connection {
+
+  
+ 
+    public static function connect(){
+       
+        try{
+            $conn = new PDO("mysql:host=".$_ENV['DB_HOST']."; dbname=".$_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'] );
+            echo "done";
+            return $conn;
+        }
+        catch(PDOException $e){
+            echo "connection failed : ".$e->getMessage();
+        }
+
     }
 
 
  }
-
-
-
 
 
 
